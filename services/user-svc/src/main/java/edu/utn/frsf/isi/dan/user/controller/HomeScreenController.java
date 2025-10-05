b03e80a2aabe18485a2615785f76b679cd7b971c
SJMedina <santinjo25@gmail.com>
2025-10-05 15:26:41 -0300

    Integración de services funcionando correctamente

---
+++ services/user-svc/src/main/java/edu/utn/frsf/isi/dan/user/controller/HomeScreenController.java
@@ -0,0 +1,15 @@
+// src/main/java/edu/utn/frsf/isi/dan/user/controller/HomeController.java
+package edu.utn.frsf.isi.dan.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeScreenController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("El sistema está corriendo correctamente.");
+    }
+}
